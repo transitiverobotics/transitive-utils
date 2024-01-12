e949f25677fdd6df96e1bce5e0e15ad68c6f754e
Christian Fritz <christian.fritz@gmail.com>
2024-01-11 17:20:12 -0800

    wip: switching utils-web to esbuild
    - seems to work (tested by manually copying over the resulting .js bundle to a cap's node_module folder)
    - not much benefit for now, webpack is pretty fast in this case
    - next step would be to replace webpack serve with https://esbuild.github.io/api/#live-reload

---
+++ web/esbuild.js
@@ -0,0 +1,47 @@
+const esbuild = require('esbuild');
+const fs = require('fs');
+
+const isDevelopment = (process.env.npm_lifecycle_event == 'dev-build');
+
+const config = {
+  entryPoints: [{in: './index.js', out: 'utils-web'}],
+  bundle: true,
+  format: 'cjs',
+  preserveSymlinks: true, // this allows us to use symlinks
+  // minify: !isDevelopment,
+  // sourcemap: isDevelopment,
+  minify: true,
+  sourcemap: false,
+  target: ['chrome110', 'firefox110', 'safari15', 'edge110'],
+  // target: ['es2022'],
+  external: ['react', 'react-dom', 'react-shadow-dom-retarget-events', 'lodash', 'mqtt-browser', '@webcomponents/shadydom', '@webcomponents/custom-elements', 'react-web-component-style-loader/exports'],
+  // external: ['./node_modules/*'],
+  outdir: 'esdist',
+  loader: {
+    '.js': 'jsx',
+    '.svg': 'text',
+    // '.wasm': 'file',
+    // '.css': 'local-css',
+  },
+  plugins: [{
+    name: 'rebuild-notify',
+    setup(build) {
+      build.onEnd(result => {
+        console.log(new Date(),
+          `build ended with ${result.errors.length} errors`);
+      })
+    },
+  }],
+};
+
+const run = async () => {
+  const ctx = await esbuild.context(config);
+  if (isDevelopment) {
+    await ctx.watch();
+  } else {
+    await ctx.rebuild();
+    process.exit(0);
+  }
+};
+
+run();
