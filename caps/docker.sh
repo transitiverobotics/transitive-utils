2a38c82c50478eaf1510fd429afb85549183e232
Christian Fritz <christian.fritz@gmail.com>
2025-05-26 11:58:59 -0700

    utils-caps@0.2.8
    - mount dev files into cloud dev container

--- caps/docker.sh
+++ caps/docker.sh
@@ -3,7 +3,7 @@
 
 # Script to run cloud caps in docker. This tries to replicate what we do in
 # cloud/app/docker.js, just for dev.
-# Usage: sym-link into cap folder, run there.
+# Usage: invoked by subScript via `npm start` (part of utils-caps)
 
 set -e
 
@@ -61,6 +61,9 @@ $TMPDIR
 mkdir -p /tmp/pers/common
 mkdir -p /tmp/pers/$TAG
 
+# bind mounts for all .js files in capability root folder
+jsFiles=$(for n in *.js; do echo -v $PWD/$n:/app/$n; done)
+
 docker run -it --rm --init \
 --env MQTT_URL=mqtts://mosquitto \
 --env PUBLIC_PORT=$PORT \
@@ -69,10 +72,11 @@ docker run -it --rm --init \
 -p $PORT:1000 -p $PORT:1000/udp \
 -v /tmp/pers/common:/persistent/common \
 -v /tmp/pers/${TAG//:/.}:/persistent/self \
+-v $PWD/cloud:/app/cloud \
+$jsFiles \
 --network=cloud_caps \
 --name $CONTAINER_NAME \
 $TAG $@
-# -v $PWD/cloud:/app/cloud \
 
 # not in use by any capabilities right now:
 # --env MIN_PORT=$MIN_PORT \
