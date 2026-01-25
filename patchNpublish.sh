3514e075508355527dc127a063b3302e9bffd0e6
Christian Fritz <christian.fritz@gmail.com>
2026-01-25 09:47:13 -0800

    Updated patch script

--- patchNpublish.sh
+++ patchNpublish.sh
@@ -2,9 +2,17 @@
 
 set -e
 
-npm version patch
+if [[ $# > 0 ]]; then
+  if ( read -p "Creating new version of type $1. Enter if sure [Ctrl-C to cancel]" ); then
+    echo 'ok';
+  fi;
+fi;
+
+TYPE=${1:-patch}
+
+npm version $TYPE
 npm publish
 
 cd web
-npm version patch
+npm version $TYPE
 npm publish
