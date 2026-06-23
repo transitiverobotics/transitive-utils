8a1ae186a1f4c62473525e596b8715a1875f0e69
Christian Fritz <christian.fritz@gmail.com>
2026-06-23 11:36:51 -0700

    commented out unused test fixture

--- test/broker.js
+++ test/broker.js
@@ -1,32 +1,34 @@
-const cluster = require('node:cluster');
-const Aedes = require('aedes');
+// NO IN USE
 
-// cluster.worker.on('message', handleProtocol);
+// const cluster = require('node:cluster');
+// const Aedes = require('aedes');
 
-const port = process.env.MQTT_PORT;
+// // cluster.worker.on('message', handleProtocol);
 
-aedes = Aedes({
-  authenticate: (client, username, password, callback) => {
-    callback(null, true);
-  },
-  authorizePublish: (client, sub, callback) => {
-    if (sub.topic.endsWith('notAllowed')) {
-      callback('not allowed', false);
-    } else {
-      callback(null, true);
-    }
-  },
-});
+// const port = process.env.MQTT_PORT;
 
-server = require('net').createServer(aedes.handle);
-server.listen(port, function () {
-  console.log('mqtt server started and listening on port ', port)
+// aedes = Aedes({
+//   authenticate: (client, username, password, callback) => {
+//     callback(null, true);
+//   },
+//   authorizePublish: (client, sub, callback) => {
+//     if (sub.topic.endsWith('notAllowed')) {
+//       callback('not allowed', false);
+//     } else {
+//       callback(null, true);
+//     }
+//   },
+// });
 
-  const start = Date.now();
-  interval = setInterval(() => aedes.publish({
-      topic: '$SYS/broker/uptime',
-      payload: String((Date.now() - start)/1e3) + ' seconds'
-    }), 200);
+// server = require('net').createServer(aedes.handle);
+// server.listen(port, function () {
+//   console.log('mqtt server started and listening on port ', port)
 
-  cluster.worker.send({event: 'onOpen'});
-});
+//   const start = Date.now();
+//   interval = setInterval(() => aedes.publish({
+//       topic: '$SYS/broker/uptime',
+//       payload: String((Date.now() - start)/1e3) + ' seconds'
+//     }), 200);
+
+//   cluster.worker.send({event: 'onOpen'});
+// });
